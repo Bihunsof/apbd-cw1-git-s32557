8ed338d8d4033adb5832d04f343cc19b932c6e58
Kostiantyn Bihun <s32557@pjwstk.edu.pl>
2026-03-19 19:57:34 +0100

    Display average in console output

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello Panie !");
+
+        int[] values = {4, 8, 2, 10, 6};
+        
+        System.out.println("Average: " + StatisticsHelper.calculateAverage(values));
+
     }
 }
